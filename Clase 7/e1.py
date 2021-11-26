98c252ccdde8f55daf293a89238b7e5efda2e800
Hetch25 <nosli_reieva@hotmail.com>
2021-11-25 23:20:44 -0600

    clase8

---
+++ Clase 7/e1.py
@@ -0,0 +1,37 @@
+#Ciclo while
+
+"""i = 0
+while i <= 10:
+    print(i)
+    i += 1"""
+"""i = 0
+list = [1,2,3,4,5,6,True]
+while i < len(list):
+    print(list[i])
+    i += 1"""
+"""count = 0
+while input("Type a message ").lower() != "stop":
+    print(count)
+    count += 1"""
+
+"""list = []
+i = 0
+another_number = True
+while another_number:
+    answer = input("Do you want another number?: ").lower()
+    if  answer == "stop":
+        another_number = False
+    elif answer == "yes":
+        list.append(i)
+        i += 1
+        print(list)
+    elif answer == "two":
+        list.extend([i,i+1])
+        print(list)
+        i +=1        
+    else:
+        print("It is not defined")
+        another_number= False"""
+#ejemplo de una matriz
+list1 = [[1,2,3],[4,5,6],[7,8,9]]
+print(list1[0][0])
